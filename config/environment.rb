a03e49890f6adf69e6dffdffa815eda2ccee6bd0
Rosalynn <admin@rosalynnashwood.com>
2018-05-15 18:00:12 -0700

    Setup mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+end
