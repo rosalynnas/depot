26fe4dd4cbb5600ff445878a72e3914e2e3789ca
Rosalynn <admin@rosalynnashwood.com>
2018-05-16 22:44:24 -0700

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login'     => :new
+    post 'login'   => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root 'store#index', as: 'store_index'
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
 
   resources :products do
     get :who_bought, on: :member
